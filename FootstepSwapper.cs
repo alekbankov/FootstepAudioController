a26f460800a67913d6af693b900e19bb8be3e885
Aleksandar Bankov <aexbankov@gmail.com>
2025-11-29 03:44:26 +0100

    Footstep Audio Controller repo

---
+++ FootstepSwapper.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class FootstepsSwapper : MonoBehaviour
+{
+    private TerrainChecker checker;
+    private string currentLayer;
+    private FootstepController _footstepController;
+    public FootstepCollection[] terrainFootstepCollection;
+    private CharacterController _characterController;
+    //private BasicMovement _movement;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        checker = new TerrainChecker();
+        _characterController = GetComponent<CharacterController>();
+        _footstepController = GetComponent<FootstepController>();
+    }
+    
+    public void CheckLayers() {
+        RaycastHit hit;
+        if(Physics.Raycast(_characterController.transform.position + Vector3.up, Vector3.down, out hit, 2)){
+            if(hit.transform.GetComponent<Terrain>() != null)
+            {
+                Terrain t = hit.transform.GetComponent<Terrain>();
+                if(currentLayer != checker.GetLayerName(_characterController.transform.position, t)){
+                    currentLayer = checker.GetLayerName(_characterController.transform.position, t);
+                    foreach(FootstepCollection collection in terrainFootstepCollection){
+                        if (currentLayer == collection.name){
+                            _footstepController.SwapFootsteps(collection);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
